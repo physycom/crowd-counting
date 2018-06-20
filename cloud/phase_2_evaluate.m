d619b629e2b303964abd6fd83cb1e021d3fa18c7
MrRomanMaccaruni <rommacc@yahoo.it>
2018-06-20 11:41:48 +0200

    add json output to cc wrapper

--- cloud/phase_2_evaluate.m
+++ cloud/phase_2_evaluate.m
@@ -1,3 +1,6 @@
+SW_VER_CROWD = 100;
+
+
 addpath('../MRF');
 MRFParams = single([105 200 1.0]);% Shanghaitech Part_A
 %MRFParams = single([200 200 8]);% Shanghaitech Part_B
@@ -34,4 +37,21 @@ end
 finalcount = sum(sum(C));
 
 disp(finalcount)
+
+tok = strsplit(base, '_');
+loctag = tok{1};
+timestamp = tok{2};
+jsonout = strcat(base, '.json');
+info_json = fopen(jsonout,'w');
+fprintf(info_json, '{\n');
+fprintf(info_json, '\t\"frame_00000\" : {\n');
+fprintf(info_json, '\t\t\"timestamp\" : %s,\n', timestamp);
+fprintf(info_json, '\t\t\"id_box\" : \"%s\",\n', loctag);
+fprintf(info_json, '\t\t\"detection\" : \"%s\",\n', "crowd2");
+fprintf(info_json, '\t\t\"sw_ver\" : %d,\n', SW_VER_CROWD);
+fprintf(info_json, '\t\t\"people_count\" : [{\"id\" : \"%s\", \"count\" : %u}],\n', loctag, finalcount);
+fprintf(info_json, '\t\t\"diagnostics\" : [{\"id\" : \"coming\", \"value\" : \"soon\"}]\n');
+fprintf(info_json, '\t}\n');
+fprintf(info_json, '}');
+
 
